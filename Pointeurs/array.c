3948b5725daaefe6ceecba0015158a88f38e59be
VM IUT <vm@iut>
2018-05-30 10:32:07 +0200

    ajout du code permettant de creer des tableaux dynamiques" ;

---
+++ Pointeurs/array.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int *create_array(int size){
+	int *array = malloc(size*sizeof(int));
+	for(int i = size; i >= 1; i--){
+		for(int j = 0; j <= size; j++){
+			array[j] = i;
+		}
+	}
+	return array;
+}
+
+void display_array(int *array, int size){
+	printf("Voici l'affichage 1 :");
+	for(int i = 0; i < size; i++){
+		printf("%d",array[i]);
+	}
+}
+
+void display_array2(int *array, int size){
+	int *fin = array + size;
+	printf("Voici l'affichage 2 : ");
+	for(int *p = array; p < fin; p++){
+		printf("%d",*p);
+	}
+}
+
+int main (void){
+	int size = 50;
+	int *tab = create_array(size);
+	display_array(tab,size);
+	display_array2(tab,size);
+	free(tab);
+	tab = NULL;
+}
