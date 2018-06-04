f7518bd287bb5fdd475b67c1a19fd67fa2fddf78
VM IUT <vm@iut>
2018-06-04 17:03:57 +0200

    Ajout des fichiers du tp7 (suite)

---
+++ Memory_leak/memory_leak.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main (void) {
+    void *p;
+    for (int i = 0; i < 50000; ++i) {
+        p = malloc(0x80000000);
+        printf("p = %p\n", p);
+        if (p == 0) {
+            printf("malloc renvoie 0.\n");
+            exit(1);
+        }
+    }
+    return 0;
+}
+
